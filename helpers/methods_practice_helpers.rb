4b34911dc76d45335455c7bc55814efa6d15bad4
galla192 <galla192@msu.edu>
2015-03-18 22:56:30 -0400

    everything should work

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -1,12 +1,14 @@
 module MethodsPracticeHelpers
 
   def image_tag(source, alternate_text)
+    return "<img src=\""+source+"\" class=\"img-responsive img-thumbnail\" alt=\""+alternate_text+"\">"
     # Generate an image tag with the given source and the classes
     # "img-responsive" and "img-thumbnail". The image should also have an alt
     # attribute with the contents of alternate_text.
   end
 
   def bootstrap_alert(message, type='info')
+    return "<div role=\"alert\" class=\"alert alert-"+type+" alert-dismissible\"><button type=\"button\" class=\"close\" aria-label=\"Close\" data-dismiss=\"alert\"><span aria-hidden=\"true\">"+"&times;"+"</span></button>"+message+"</div>"
     # Generates the HTML for a Bootstrap alert (useful since it can be diffcult
     # to remember all HTML needed to do it right). It should show the message in
     # the "message" variable and should have a class appropriate for the "type"
@@ -14,6 +16,8 @@ module MethodsPracticeHelpers
   end
 
   def current_date_and_time
+    curTime = Time.now
+    return curTime.strftime("%-B %-e, %-Y at %-l:%M%P")
     # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
     # Time.now.strftime is a great method chain you can use for this purpose.
     # See how to use it at: http://apidock.com/ruby/Time/strftime
